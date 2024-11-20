b6627040071f4a63cd2b949c4952acf41047f865
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2024-11-20 12:32:06 +0600

    Add files via upload

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOQ5H_Q2qSLw_YT_wuNP_elm1U2HtkBVs",
+  authDomain: "dragon-news-6461b.firebaseapp.com",
+  projectId: "dragon-news-6461b",
+  storageBucket: "dragon-news-6461b.firebasestorage.app",
+  messagingSenderId: "473308320379",
+  appId: "1:473308320379:web:797d88b4affaadbf40c8de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,41 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+//import { result } from "postcss";
+
 
 const Register = () => {
+    const {createNewUser,setUser}=useContext(AuthContext)
+
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        //get form data
+        const form=new FormData(e.target);
+        const name=form.get("name");
+        const email=form.get("email");
+        const photo=form.get("photo");
+        const password=form.get("password");
+        console.log({name,email,photo,password});
+        
+        createNewUser(email,password)
+        .then((result)=>{
+            const user=result.user;
+            setUser(user);
+            console.log(user);
+        })
+        .catch((error)=>{
+            const errorCode=error.code;
+            const errorMessage=error.message;
+            console.log(errorCode,errorMessage);
+        });
+    };
+
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <form className="card-body">
+                
+                <form onSubmit={handleSubmit} className="card-body">
                     <div className="form-control">
                         <h2 className="text-2xl font-semibold text-center">Register your account</h2>
                     </div>
@@ -13,28 +44,56 @@ const Register = () => {
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="Your Name" className="input input-bordered" required />
+                        
+                        <input 
+                        name="name"
+                        type="text" 
+                        placeholder="Your Name" 
+                        className="input input-bordered" 
+                        required />
+
                     </div>
                     {/* Photo URL Field */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Photo URL</span>
                         </label>
-                        <input type="url" placeholder="Photo URL" className="input input-bordered" required />
+                        
+                        <input 
+                        type="url" 
+                        name="photo"
+                        placeholder="Photo URL" 
+                        className="input input-bordered" 
+                        required />
+
                     </div>
                     {/* Email Field */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+
+                        <input 
+                        name="email"
+                        type="email" 
+                        placeholder="email" 
+                        className="input input-bordered" 
+                        required />
+                    
                     </div>
                     {/* Password Field */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        
+                        <input 
+                        name="password"
+                        type="password" 
+                        placeholder="password" 
+                        className="input input-bordered" 
+                        required />
+                        
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
